3597b5ceb2e99782369d44acc3d231d559efcf58
James Kyburz <james.kyburz@gmail.com>
2012-02-01 11:01:27 +0100

    inline-templates

---
+++ README.md
@@ -0,0 +1,26 @@
+inline-templates adds a helper function called templates for your layout
+file.
+
+``` javascript
+templates([options={type: 'text/x-template', template_path:
+'./templates/'}]);
+```
+
+## Example
+
+app.js
+``` javascript
+  require('inline-templates');
+```
+
+layout.jade
+``` slim
+  !=templates({type:'text/x-serenade'});
+```
+
+Install it via npm:
+
+```
+npm install inline-templates
+```
+
---
+++ inline-templates.js
@@ -0,0 +1,22 @@
+var fs, glob, _;
+glob = require('glob');
+fs = require('fs');
+_ = require('underscore');
+global.templates = function(options) {
+  var files, templates;
+  if (options == null) {
+    options = {};
+  }
+  options = _.extend({
+    template_path: './templates/',
+    type: 'text/x-template'
+  }, options);
+  files = glob.sync("" + options.template_path + "/*");
+  templates = _.reduce(files, function(result, fn) {
+    var id;
+    id = fn.toString().replace(options.template_path, '');
+    result.push("<script id=\"" + id + "\" type=\"" + options.type + "\">\n" + (fs.readFileSync(fn)) + "\n</script>");
+    return result;
+  }, []);
+  return templates.join("\n");
+};
